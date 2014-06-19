64e3c4bb40a3ee95f92671d87dd31d3546b0b0aa
Daniel Petti <djpetti@gmail.com>
2014-06-19 11:21:55 -0700

    Add some experimental CNN stuff.
    Now that I was getting some results out of my code, I thought it
    wise to put it in the git repo.

---
+++ CNN/cnn_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from PIL import Image
+from weight_sharing import Cnn
+
+import numpy
+import pylab
+
+img = Image.open(open("tux.jpg"))
+img = numpy.asarray(img, dtype = "float64") / 256
+width = img.shape[1]
+height = img.shape[0]
+print "Width: %d, Height: %d" % (width, height)
+
+# Put image in 4d tensor.
+img = img.swapaxes(0, 2).swapaxes(1, 2).reshape(1, 3, width, height)
+
+cnn = Cnn(img.shape, [(2, 3, 9, 9)], layers = (948832, 10, 10), activation = "sigmoid")
+print cnn.predict(img)
---
+++ CNN/weight_sharing.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+from theanets import feedforward
+from theano.tensor.nnet import conv
+from theano.tensor.signal import downsample
+
+import numpy as np
+import theano
+import theano.tensor as TT
+
+# A class for representing a neural network with weight sharing.
+class Cnn(feedforward.Classifier):
+  def __init__(self, img_shape, filter_shapes, pool_sizes = "default", *args, **kwargs):
+    self.img_shape = img_shape
+    self.conv_weights = []
+    self.biases = []
+    self.filter_shapes = []
+    self.pool_sizes = []
+    self.pool_mult_biases = []
+    self.pool_add_biases = []
+
+    if pool_sizes != "default":
+      for shape, size in zip(filter_shapes, pool_sizes):
+        self._add_layer_pair(shape, size)
+    else:
+      for shape in filter_shapes:
+        self._add_layer_pair(shape, (2, 2))
+
+    self._make_graph()
+
+    super(Cnn, self).__init__(*args, no_x = True, **kwargs)
+
+  def _add_layer_pair(self, filter_shape, pool_size):
+    # Check for the same number of feature maps.
+    if filter_shape[1] != self.img_shape[1]:
+      raise RuntimeError("Number of input feature maps must be the same.")
+    self.filter_shapes.append(filter_shape)
+
+    # Initialize weight (kernel) values.
+    fan_in = np.prod(filter_shape[1:])
+    rng = np.random.RandomState(23455)
+    weight_values = np.asarray(rng.uniform(
+        low = -np.sqrt(3. / fan_in),
+        high = np.sqrt(3. / fan_in),
+        size = filter_shape), dtype = theano.config.floatX)
+    weights = theano.shared(value = weight_values, name = "weights")
+    self.conv_weights.append(weights)
+
+    # Initialize the biases.
+    bias_values = np.zeros((filter_shape[0],), dtype = theano.config.floatX)
+    biases = theano.shared(value = bias_values, name = "biases")
+    self.biases.append(biases)
+
+    # Set up subsampling parameters.
+    self.pool_sizes.append(pool_size)
+
+  # Builds a graph for the CNN.
+  def _make_graph(self):
+    self._inputs = TT.dtensor4("inputs")
+    layer_outputs = self._inputs
+    for i in range(0, len(self.conv_weights)):
+      # Perform the convolution.
+      # TODO (danielp): img_shape is going to need to be changed.
+      conv_out = conv.conv2d(layer_outputs, self.conv_weights[i],
+          filter_shape = self.filter_shapes[i],
+          image_shape = self.img_shape)
+
+      # Downsample the feature maps.
+      pooled_out = downsample.max_pool_2d(conv_out, self.pool_sizes[i],
+          ignore_border = True)
+
+      # Account for the bias. Since it is a vector, we first need to reshape it
+      # to (1, n_filters, 1, 1).
+      layer_outputs = TT.nnet.sigmoid(pooled_out + \
+          self.biases[i].dimshuffle("x", 0, "x", "x"))
+
+    # Concatenate output maps into one long vector and set it as the input for
+    # the normal part of our network.
+    self.x = TT.flatten(layer_outputs)
+
+  def _compile(self):
+    self._compute = theano.function([self._inputs], self.hiddens + [self.y])
+
+  def predict(self, inputs):
+    self._compile()
+    return self._compute(inputs)
+
+  @property
+  def params(self):
+    params = super(feedforward.Classifier, self).params
+    params.extend(self.conv_weights)
+    params.extend(self.biases)
